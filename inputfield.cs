ad7f67bf174c2ab406e13ac24f7279083d0ff90b
JOSH-Dr1fter <121372944+JOSH-Dr1fter@users.noreply.github.com>
2022-12-25 12:07:27 +0500

    Add files via upload

---
+++ inputfield.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class inputfield : MonoBehaviour
+{
+    public RCC_AIWaypointsContainer waypointsContainer;
+    public string input_location;
+    public string location = "Anywhere";
+    void ReadStringInput(string input_location){
+        if (input_location == location)
+            {
+//               waypointsContainer = FindObjectOfType(typeof(RCC_AIWaypointsContainer)) as RCC_AIWaypointsContainer;
+            }
+        }
+}
+
+
+//Hali bu kod ishlamaydi
